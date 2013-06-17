1bb535712054323a5dc0f272d9cf2eae7e99f8c6
SneakyBrian <SneakyBrian@gmail.com>
2013-06-18 00:22:52 +0100

    Started to add desktop app

---
+++ AutoFileMover.Desktop/ViewModels/Main/EngineViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoFileMover.Core;
+using ReactiveUI;
+
+namespace AutoFileMover.Desktop.ViewModels.Main
+{
+    public class EngineViewModel : ReactiveObject
+    {
+        public IObservable<FileEventArgs> FileDetected { get; private set; }
+
+        public EngineViewModel(IEngine engine)
+        {
+            FileDetected = Observable.FromEventPattern<EventHandler<FileEventArgs>, FileEventArgs>(h => engine.FileDetected += h, h => engine.FileDetected -= h)
+                                        .Select(e => e.EventArgs);
+
+            FileDetected.Subscribe(e => this.RaisePropertyChanged("FileDetected"));
+                
+        }
+
+    }
+}
