96d75cadd5a3a9c7fc22039bc0606776d711f808
SneakyBrian <SneakyBrian@gmail.com>
2013-07-03 23:17:27 +0100

    updated publish script to produce better markdown

--- build/process_publish.bat
+++ build/process_publish.bat
@@ -1,6 +1,6 @@
 @echo off
 
-pandoc -f html -t markdown -o index_temp.md ..\downloads\latest\publish.html
+pandoc -f html -t markdown-simple_tables-pipe_tables -o index_temp.md ..\downloads\latest\publish.html
 
 copy header.md + index_temp.md ..\downloads\latest\index.md"
 
